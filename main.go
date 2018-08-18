641aaef61c2a346969be99a19dbc11d6d9be3f36
Чалый Роман Александрович <cerebrum.ch@gmail.com>
2018-08-19 04:01:18 +0700

    add ignore

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"os"
+	"log"
 )
 
 func main() {
@@ -13,6 +15,12 @@ func main() {
 	end, _ := time.Parse("2006-01-02 15:03:05", "2018-10-30 00:00:00")
 	b := parse(begin, end)
 	fmt.Println(b)
+	file := create(b)
+	w, err := os.Create("out.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Write(w)
 	http.HandleFunc("/", ProcessedHandle)
 	http.ListenAndServe(":8080", nil)
 }
